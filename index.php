122bcac9881ec6b1ed858ace091354130d049ad7
Edward Foyle <efoyle3@gatech.edu>
2017-03-04 19:57:19 -0500

    added stuff for hosting

---
+++ index.php
@@ -0,0 +1,50 @@
+<?php
+
+$params = strtok($_SERVER['REQUEST_URI'], '?');
+$params = explode("/", $params);
+$params = array_splice($params,1);
+//$params = array_map('strtolower', $params);
+
+array_shift($params);
+if(empty(end($params))){
+    array_pop($params);
+}
+
+if (strcmp($params[0], "home")==0) {
+    include("home_screen.html");
+} else if (strcmp($params[0], "login")==0) {
+    include("login_page.html");
+}
+
+// if(count($params)==3){
+//     $focus_topic = $params[0];
+//     $question_type = $params[1];
+//     $sequence_number = intval($params[2]);
+//     $question_list = getQuestionList($conn);
+//     $question_data = getQuestion($conn, $focus_topic, $question_type, $sequence_number);
+//     include("./content/multiple_choice.php");
+// } else if(count($params)>1 && strcmp($params[0], "focus")==0) {
+//     $focus_topic = $params[1];
+//     if(count($params)==4){
+//         $seq_num = intval($params[3]);
+//     }else{
+//         $seq_num = 1;
+//     }
+//     $data = getFocusList($conn, $focus_topic, $seq_num);
+//     $focus_list = $data["results"];
+//     $question_id = $data["question_id"];
+//     $prev_question = $data["prev_question"];
+//     $next_question = $data["next_question"];
+//     $rand_question = $data["rand_question"];
+
+//     $question_data = getFocusQuestion($conn, $question_id, $seq_num, $focus_topic, $prev_question, $next_question, $rand_question);
+//     $focus_options = getFocusOptions($conn);
+//     include("./content/focus.php");
+// } else {
+//     $question_list = getQuestionList($conn);
+//     include("./content/home.php");
+// }
+
+// killConnection($conn);
+
+?>
